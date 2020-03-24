268b3bd02eb37e1d287f24b7e8f7c96aa2bd6bfe
AlexXer <alexxerman@gmail.com>
2020-03-24 20:23:00 +0300

    add solutions for the fourth and the fifth tasks

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,35 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var output = mutableListOf<String>()
+
+        for (i in inputString.indices){
+            when(inputString[i]){
+                '<' -> output.add(getSubstring(inputString.substring(i), '<', '>'))
+                '(' -> output.add(getSubstring(inputString.substring(i), '(', ')'))
+                '[' -> output.add(getSubstring(inputString.substring(i), '[', ']'))
+            }
+        }
+        return output.toTypedArray()
     }
+
+    fun getSubstring(string: String, start: Char, end: Char): String{
+        var res = ""
+        var count = 0
+
+        for (i in 1 .. string.length) {
+            if (string[i] == start)
+                count++
+            else if (string[i] == end && count != 0)
+                count--
+            else if (string[i] == end && count == 0) {
+                res = string.substring(1, i)
+                break
+            }
+        }
+
+        return res
+    }
+
+
 }
--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,52 @@
 package subtask5
 
+
+import java.lang.Integer.max
+import java.lang.StringBuilder
+
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val res = StringBuilder(digitString)
+        val needToChange = BooleanArray(n / 2)
+        var numberChanges = k
+
+        for (i in 0 until res.length / 2) {
+            if (res[i] != res[n - 1 - i]) {
+                numberChanges--
+                needToChange[i] = true
+            }
+        }
+
+        if (numberChanges >= 0) {
+            for (i in 0 until res.length / 2) {
+                if (!needToChange[i]) {
+                    if (numberChanges >= 2 && res[i] != '9') {
+                        equal(res, i, '9')
+                        numberChanges -= 2
+                    }
+                } else {
+                    if (res[i] != '9' && res[n - i - 1] != '9' && numberChanges >= 1) {
+                        equal(res, i, '9')
+                        numberChanges--
+                    } else {
+                        equal(res, i,
+                            max(res[i].toInt(), res[n - i - 1].toInt()).toChar())
+                    }
+                }
+            }
+            if (numberChanges >= 1) {
+                res[n / 2] = '9'
+            }
+            return res.toString()
+        } else {
+            return "-1"
+        }
+    }
+
+    private fun equal(string: StringBuilder, index: Int, value: Char) {
+        string[index] = value
+        string[string.length - 1 - index] = value
     }
 }
